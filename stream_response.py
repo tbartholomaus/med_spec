37ba53c3f6a52d66024014d15087800e34e80628
Timothy Bartholomaus <tbartholomaus@kennicott.ibest.uidaho.edu>
2018-11-20 09:41:39 -0800

    Modest changes for testing and saving output

--- stream_response.py
+++ stream_response.py
@@ -9,9 +9,15 @@ Created on Fri Nov 16 16:58:54 2018
 import obspy
 from clone_inv import clone_inv
 
+import numpy as np
+
 network= 'XX'
 station= 'SE47'
 channel= 'HHZ'
+
+pre_filt = (0.1, .2, 90, 100.)
+
+# %%
 
 # include full paths to data, with final forward slash: /
 # script assumes miniseed files are organized in data_dir + station/
@@ -20,10 +26,15 @@ data_dir = '/data/stor/basic_data/seismic_data/day_vols/MoVE/'
 # include full path to resp file
 resp_file = '/data/stor/basic_data/seismic_data/day_vols/MoVE/Resp/XX.UI05.resp/XX.UI05.HHZ.resp'
 
-%timeit st = obspy.read(data_dir + station + '/SE47.XX..HHZ.2018.130')
+#%timeit
+st = obspy.read(data_dir + station + '/SE47.XX..HHZ.2018.130')
 
 
-%timeit 
+#%timeit 
 inv = obspy.read_inventory(resp_file)
 inv = clone_inv(inv, network, station)
 inv = inv.select(channel=channel, station=station) # subset the inventory to just that which is necessary for script
+
+#%timeit st_IC = st.copy().remove_response(inventory=inv, output="VEL", pre_filt=pre_filt)
+
+#%timeit fft_out = np.fft.fft(st[0].data)
